243e36199b60f0825071a543d462d53b5a70eb93
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-03-24 23:04:07 +0530

    responcive

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,6 +8,11 @@ export default function Home() {
     <div>
       <Navbar />
       <Heros />
+      <div className="w-full h-36  text-2xl sm:text-5xl flex justify-center text-shadowed font-poppins items-center">
+        About{" "}
+        <p className="text-[#8d8b00] pl-3 font-bold text-shadowed"> Gini</p>
+        Market
+      </div>
     </div>
   );
 }
